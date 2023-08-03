3a99ff06da1df6dd696dcdbbc03c5014e9f8a709
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2023-08-03 18:30:48 +0530

    validation middleware changes

--- Middlewares/validation.middleware.js
+++ Middlewares/validation.middleware.js
@@ -6,7 +6,7 @@ const validation = (schema) => {
             next(); 
         } catch (error) {
             const errors = error.inner.map(err => err.message); // Extract error messages
-            res.status(400).json( errors);
+            res.status(400).json( {"error":errors});
         }
     };
 };
--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const app = express()
 
 app.use(express.json())
 
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 
 app.get('/', (req, res) => {
