7e29860ebe91cd5e9b9f2a5faacf005773082bc4
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2023-08-03 17:48:01 +0530

    error handling done

--- Middlewares/validation.middleware.js
+++ Middlewares/validation.middleware.js
@@ -2,10 +2,11 @@ const validation = (schema) => {
     return async (req, res, next) => {
         const body = req.body;
         try {
-            await schema.validate(body);
+            await schema.validate(body, { abortEarly: false }); // Collect all validation errors
             next(); 
         } catch (error) {
-            res.status(400).json({error : error.errors[0]});
+            const errors = error.inner.map(err => err.message); // Extract error messages
+            res.status(400).json( errors);
         }
     };
 };
